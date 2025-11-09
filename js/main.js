ca1d903d8005e0a22820d6ef2d63ed98872fbf84
Sushant Sejwal <sushantsejwal19@gmail.com>
2025-11-09 23:33:24 +0530

    commit before final commit

--- js/main.js
+++
@@ -1,8 +0,0 @@
-const hamMenuBtn = document.getElementById("ham-menu");
-const hamMenuLines = document.querySelectorAll(".line");
-hamMenuBtn.addEventListener("click", () => {
-    //  hamMenuLines
-    hamMenuLines.forEach( ele => {
-        ele.classList.toggle("clicked");
-    });
-})
